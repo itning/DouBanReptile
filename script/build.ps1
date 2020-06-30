684012726090bae577dfb409c83fc700f416dbc3
itning <itning@itning.top>
2020-06-30 12:29:54 +0800

    chore: move main.go file

--- script/build.ps1
+++ script/build.ps1
@@ -1,3 +1,3 @@
-go build -ldflags="-s -w -H windowsgui" -o ..\bin\main.exe github.com/itning/DouBanReptile/cmd
+go build -ldflags="-s -w -H windowsgui" -o ..\bin\main.exe github.com/itning/DouBanReptile
 echo SUCCESS
 pause
